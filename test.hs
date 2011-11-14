5710a78f9a961c1912c52e7e8dfbe33629ee6799
Hirohito Sasakawa <hiro.sassa@gmail.com>
2011-11-14 12:13:43 +0900

    Chapter3
    I've read chapter 3!!

---
+++ test.hs
@@ -0,0 +1,6 @@
+double x = x + x
+quadruple x = double (double x)
+-- Calc factrial of a positive integers
+fac n = product [1 .. n]
+-- Acerage of a list of integers:
+average ns = sum ns `div` length ns
